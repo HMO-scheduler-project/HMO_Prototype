d9441f5df1db0493fb0108480d6967b7a7364481
Shir <Shirsneh54@gmail.com>
2021-12-11 21:29:59 +0200

    updated

--- client/loginController.java
+++ client/loginController.java
@@ -14,6 +14,7 @@ import java.security.NoSuchAlgorithmException;
 
 /** Controls the login screen */
 public class loginController {
+
     private SimpleClient client;
     private static int sessionID = 0;
 
